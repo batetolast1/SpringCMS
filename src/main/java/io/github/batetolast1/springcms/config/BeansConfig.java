0ca396a8b8e70cc9329e038d9d6f9464a3fb111e
batetolast1 <batetolast1@gmail.com>
2020-11-10 17:07:52 +0100

    feature: added Hibernate Validator support

--- src/main/java/io/github/batetolast1/springcms/config/BeansConfig.java
+++ src/main/java/io/github/batetolast1/springcms/config/BeansConfig.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
 import java.util.Locale;
 
 @Configuration
@@ -57,5 +59,12 @@ public class BeansConfig {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
         localeResolver.setDefaultLocale(new Locale("pl", "PL"));
         return localeResolver;
+    }
+
+    // Hibernate Validator
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
     }
 }
